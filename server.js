68bfbd9a37d87eb89625b2b3fbcc6cc183f26a29
fateeq <44147888+fateeq@users.noreply.github.com>
2019-04-26 15:34:54 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,54 @@
+// import npm packages
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+// import end-points
+const signIn = require('./controllers/signIn');
+const register = require('./controllers/register');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+// import database
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : 'process.env.DATABASE_URL',
+    ssl: true,
+  }
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+	res.send(database.users);
+})
+
+app.post('/signin', (req, res) => { 
+	signIn.handleSignIn(req, res, db, bcrypt) 
+})
+
+app.post('/register', (req, res) => { 
+	register.handleRegister(req, res, db, bcrypt) 
+})
+
+app.get('/profile/:id', (req, res) => {
+	profile.handleProfile(req, res, db)
+})
+
+app.put('/image', (req, res) => {
+	image.handleImage(req, res, db)
+})
+
+app.post('/imageUrl', (req, res) => {
+  image.handleApiCall(req, res)
+})
+
+app.listen(process.env.PORT || 3001, () => {
+	console.log(`app is running on port ${process.env.PORT}`);
+})
